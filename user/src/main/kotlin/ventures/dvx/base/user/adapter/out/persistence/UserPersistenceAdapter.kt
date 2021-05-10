17a6de22eeb43e7c6e9b52057a8a2712317301b9
Erik Dreyer <erik.dreyer@gmail.com>
2021-05-10 17:37:53 -0400

    refactoring User domain object

--- user/src/main/kotlin/ventures/dvx/base/user/adapter/out/persistence/UserPersistenceAdapter.kt
+++ user/src/main/kotlin/ventures/dvx/base/user/adapter/out/persistence/UserPersistenceAdapter.kt
@@ -1,25 +1,13 @@
 package ventures.dvx.base.user.adapter.out.persistence
 
-import arrow.core.NonEmptyList
-import ventures.dvx.base.user.application.port.out.SaveNewUserPort
 import ventures.dvx.base.user.application.port.out.FindUserPort
+import ventures.dvx.base.user.application.port.out.SaveNewUserPort
 import ventures.dvx.base.user.domain.ActiveUser
+import ventures.dvx.base.user.domain.AdminUser
 import ventures.dvx.base.user.domain.UnregisteredUser
 import ventures.dvx.base.user.domain.User
 import ventures.dvx.common.logging.LoggerDelegate
 import ventures.dvx.common.mapping.DataClassMapper
-
-fun Role.toDomain() = ventures.dvx.base.user.domain.Role.valueOf(this.toString())
-
-fun ventures.dvx.base.user.domain.Role.toJpa() = Role.valueOf(this.toString())
-
-fun NonEmptyList<ventures.dvx.base.user.domain.Role>.toJpa() = this.map {
-  it.toJpa()
-}.toList()
-
-fun List<Role>.toDomain() = NonEmptyList.fromListUnsafe(this.map {
-  it.toDomain()
-})
 
 class UserPersistenceAdapter(
   private val userRepository: UserRepository
@@ -28,49 +16,64 @@ class UserPersistenceAdapter(
   private val logger by LoggerDelegate()
 
   override fun findUserByUsername(username: String) : User? =
-    userRepository.findByUsername(username)?.toActiveUser()
+    userRepository.findByUsername(username)?.toUser()
 
   override fun findUserByEmail(email: String) : User? =
-    userRepository.findByEmail(email)?.toActiveUser()
+    userRepository.findByEmail(email)?.toUser()
 
   override fun saveNewUser(user: UnregisteredUser): ActiveUser =
-    userRepository.save(user.toEntity()).toActiveUser()
+    when(val newUser = userRepository.save(user.toEntity()).toUser()) {
+      is ActiveUser -> newUser
+      else -> {
+        val err = ""
+        logger.error(err)
+        throw IllegalStateException(err)
+      }
+    }
 
-  private fun UserEntity.toActiveUser() : ActiveUser = ActiveUser.of(
-    this.username, this.email, this.password, this.roles.toDomain())
-    .fold(
-      { errors ->
-        val errorMsg = errors.map { it.toString() }.joinToString { "\n" }
-        logger.error(errorMsg)
-        throw IllegalStateException(errorMsg)
-      },
-      { activeUser -> activeUser }
+  private fun UserEntity.toUser() : User = when {
+    Role.ROLE_USER in this.roles -> ActiveUser.of(
+      this.username, this.email, this.password
     )
-
-  private fun User.toEntity() : UserEntity = when (this) {
-    is ActiveUser -> {
-      val activeUserToEntityMapper = DataClassMapper<ActiveUser, UserEntity>()
-        .targetParameterSupplier(UserEntity::roles) { this.roles.toJpa()}
-        .targetParameterSupplier(UserEntity::password) { this.encryptedPassword.value }
-        .targetParameterSupplier(UserEntity::email) { this.email.value}
-        .targetParameterSupplier(UserEntity::username) {this.username.value}
-        .targetParameterSupplier(UserEntity::active) { true }
-
-      userRepository.findByUsername(this.username.value)
-        ?.copy(email = this.email.value, password = this.encryptedPassword.value, roles = this.roles.toJpa())
-        ?: activeUserToEntityMapper(this)
+      .fold(
+        { errors ->
+          val err = errors.map { it.toString() }.joinToString { "\n" }
+          logger.error(err)
+          throw IllegalStateException(err)
+        },
+        { user -> user }
+      )
+    Role.ROLE_ADMIN in this.roles -> AdminUser.of(
+      this.username, this.email, this.password
+    )
+      .fold(
+        { errors ->
+          val err = errors.map { it.toString() }.joinToString { "\n" }
+          logger.error(err)
+          throw IllegalStateException(err)
+        },
+        { user -> user }
+      )
+    else -> {
+      val err = "Unknown User Type. roles=${this.roles}"
+      logger.error(err)
+      throw IllegalStateException(err)
     }
-    is UnregisteredUser -> {
-      val role = Role.ROLE_USER
-      val unregisteredUserToEntityMapper = DataClassMapper<UnregisteredUser, UserEntity>()
-        .targetParameterSupplier(UserEntity::username) { this.username.value }
-        .targetParameterSupplier(UserEntity::email) { this.email.value }
-        .targetParameterSupplier(UserEntity::password) { this.encryptedPassword.value }
-        .targetParameterSupplier(UserEntity::roles) { listOf(role) }
-        .targetParameterSupplier(UserEntity::active) { true }
+  }
 
-      unregisteredUserToEntityMapper(this)
-    }
+  private fun User.toEntity() : UserEntity {
+    val mapper = DataClassMapper<User, UserEntity>()
+      .targetParameterSupplier(UserEntity::username) { this.username.value }
+      .targetParameterSupplier(UserEntity::email) { this.email.value }
+      .targetParameterSupplier(UserEntity::password) { this.encryptedPassword.value }
+      .targetParameterSupplier(UserEntity::roles) { when (this) {
+        is AdminUser -> listOf(Role.ROLE_ADMIN)
+        else -> listOf(Role.ROLE_USER)
+      }}
+      .targetParameterSupplier(UserEntity::active) { true }
+    return userRepository.findByUsername(this.username.value)
+      ?.copy(email = this.email.value, password = this.encryptedPassword.value)
+      ?: mapper(this)
   }
 
 }
--- user/src/main/kotlin/ventures/dvx/base/user/domain/User.kt
+++ user/src/main/kotlin/ventures/dvx/base/user/domain/User.kt
@@ -1,50 +1,68 @@
 package ventures.dvx.base.user.domain
 
-import arrow.core.Nel
-import arrow.core.NonEmptyList
 import arrow.core.ValidatedNel
-import arrow.core.nel
 import arrow.core.zip
 import ventures.dvx.common.types.EmailAddress
 import ventures.dvx.common.types.NonEmptyString
 import ventures.dvx.common.types.ValidationError
 
-enum class Role {
-  ROLE_USER,
-  ROLE_ADMIN
+sealed class User {
+  abstract val username: NonEmptyString
+  abstract val email: EmailAddress
+  abstract val encryptedPassword: NonEmptyString
 }
 
-sealed class User
-
 data class UnregisteredUser(
-  val username: NonEmptyString,
-  val email : EmailAddress,
-  val encryptedPassword: NonEmptyString
+  override val username: NonEmptyString,
+  override val email : EmailAddress,
+  override val encryptedPassword: NonEmptyString
 ) : User() {
   companion object {
-    fun of(username: String, email: String, encryptedPassword: String)
-      : ValidatedNel<ValidationError, UnregisteredUser> =
-      NonEmptyString.of(username).zip(
-        EmailAddress.of(email),
-        NonEmptyString.of(encryptedPassword)
-      ) {u, e, p -> UnregisteredUser(u, e, p)}
+    fun of(username: String, email: String, encryptedPassword: String):
+      ValidatedNel<ValidationError, UnregisteredUser> =
+      validateAndCreate(username, email, encryptedPassword) {
+        u, e, p -> UnregisteredUser(u, e, p)
+      }
   }
 }
 
 data class ActiveUser(
-  val username: NonEmptyString,
-  val email: EmailAddress,
-  val encryptedPassword: NonEmptyString,
-  val roles: NonEmptyList<Role>
+  override val username: NonEmptyString,
+  override val email: EmailAddress,
+  override val encryptedPassword: NonEmptyString,
 ) : User() {
   companion object {
-
-    fun of(username: String, email: String, encryptedPassword: String, roles: Nel<Role> = Role.ROLE_USER.nel())
-      : ValidatedNel<ValidationError, ActiveUser> =
-      NonEmptyString.of(username).zip(
-        EmailAddress.of(email),
-        NonEmptyString.of(encryptedPassword)
-      ) {u, e, p -> ActiveUser(u, e, p, roles)}
-
+    fun of(username: String, email: String, encryptedPassword: String):
+      ValidatedNel<ValidationError, ActiveUser> =
+      validateAndCreate(username, email, encryptedPassword) {
+        u, e, p -> ActiveUser(u, e, p)
+      }
   }
 }
+
+data class AdminUser(
+  override val username: NonEmptyString,
+  override val email: EmailAddress,
+  override val encryptedPassword: NonEmptyString,
+) : User() {
+  companion object {
+    fun of(username: String, email: String, encryptedPassword: String):
+      ValidatedNel<ValidationError, AdminUser> =
+      validateAndCreate(username, email, encryptedPassword) {
+        u, e, p -> AdminUser(u, e, p)
+      }
+  }
+}
+
+// DRYs up object creation
+private fun <T> validateAndCreate(
+  username: String,
+  email: String,
+  encryptedPassword: String,
+  createFn: (un: NonEmptyString, em: EmailAddress, pa: NonEmptyString) -> T) :
+  ValidatedNel<ValidationError, T> =
+  NonEmptyString.of(username).zip(
+    EmailAddress.of(email),
+    NonEmptyString.of(encryptedPassword),
+    createFn
+  )
