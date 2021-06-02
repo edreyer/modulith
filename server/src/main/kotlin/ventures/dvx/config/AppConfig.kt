75b42485656ac2f7f87c607c99aa2f6902c05ad9
Erik Dreyer <erik.dreyer@gmail.com>
2021-06-02 16:50:04 -0400

    switching to Spring Data JDBC

--- server/src/main/kotlin/ventures/dvx/config/AppConfig.kt
+++ server/src/main/kotlin/ventures/dvx/config/AppConfig.kt
@@ -2,11 +2,13 @@ package ventures.dvx.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
 
 import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
+@EnableJdbcRepositories
 class AppConfig {
 
   @Bean
