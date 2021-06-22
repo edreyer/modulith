63b69021414336527c5d5447412a97bdde92c57e
Erik Dreyer <erik.dreyer@gmail.com>
2021-06-22 12:52:06 -0400

    now contains all axon DB tables

--- server/src/main/resources/db/migration/V1__baseline.sql
+++ server/src/main/resources/db/migration/V1__baseline.sql
@@ -1,16 +1,39 @@
 
 create schema if not exists public;
 
--- Required for Axon's JpaTokenStore
-create table if not exists token_entry
+CREATE TABLE IF NOT EXISTS token_entry
 (
-    processor_name varchar(255)
-        constraint token_entry_pk
-            primary key,
-    segment int,
-    token text,
-    token_type varchar(255),
-    timestamp varchar(255),
-    owner varchar(255)
+    processor_name VARCHAR(255) NOT NULL,
+    segment INTEGER NOT NULL,
+    owner VARCHAR(255),
+    timestamp VARCHAR(255) NOT NULL,
+    token oid,
+    token_type VARCHAR(255),
+    PRIMARY KEY (processor_name, segment)
 );
 
+-- Required for Axon's JpaTokenStore
+CREATE TABLE IF NOT EXISTS association_value_entry
+(
+    id SERIAL,
+    association_key VARCHAR(255) NOT NULL,
+    association_value VARCHAR(255),
+    saga_id VARCHAR(255) NOT NULL,
+    saga_type VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+CREATE INDEX IF NOT EXISTS association_value_entry_fields_idx
+    on association_value_entry (saga_type, association_key, association_value);
+
+CREATE INDEX IF NOT EXISTS association_value_entry_saga_id_saga_type_idx
+    on association_value_entry (saga_id, saga_type);
+
+CREATE TABLE IF NOT EXISTS saga_entry
+(
+    saga_id VARCHAR(255) NOT NULL,
+    revision VARCHAR(255),
+    saga_type VARCHAR(255),
+    serialized_saga oid,
+    PRIMARY KEY (saga_id)
+);
