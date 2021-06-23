cd3840668b9201326ebbe153bbee8b4d89d9c911
Erik Dreyer <erik.dreyer@gmail.com>
2021-06-23 11:24:43 -0400

    revert to Xstream for serialization

--- server/src/main/kotlin/ventures/dvx/config/AxonConfig.kt
+++ server/src/main/kotlin/ventures/dvx/config/AxonConfig.kt
@@ -1,20 +1,12 @@
 package ventures.dvx.config
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.axonframework.commandhandling.CommandBus
 import org.axonframework.config.EventProcessingConfigurer
 import org.axonframework.eventhandling.EventBus
 import org.axonframework.messaging.Message
 import org.axonframework.messaging.interceptors.LoggingInterceptor
 import org.axonframework.queryhandling.QueryBus
-import org.axonframework.serialization.Serializer
-import org.axonframework.serialization.json.JacksonSerializer
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -60,21 +52,6 @@ class AxonConfig {
   ) {
     queryBus.registerDispatchInterceptor(loggingInterceptor)
     queryBus.registerHandlerInterceptor(loggingInterceptor)
-  }
-
-  @Bean
-  @Qualifier(value = "messageSerializer")
-  fun messageSerializer(): Serializer {
-    val mapper = ObjectMapper()
-    mapper.registerModule(JavaTimeModule())
-      .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-      .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
-    mapper.registerModule(Jdk8Module())
-    mapper.registerModule(KotlinModule())
-    return JacksonSerializer.builder()
-      .objectMapper(mapper)
-      .lenientDeserialization()
-      .build()
   }
 
 }
