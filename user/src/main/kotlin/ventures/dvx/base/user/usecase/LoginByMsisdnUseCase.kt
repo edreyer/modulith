f85a208218f619181743946e2658b8cf30886878
Erik Dreyer <erik.dreyer@gmail.com>
2021-09-01 17:28:08 -0400

    Refactored login use case to call sub usecase

--- user/src/main/kotlin/ventures/dvx/base/user/usecase/LoginByMsisdnUseCase.kt
+++ user/src/main/kotlin/ventures/dvx/base/user/usecase/LoginByMsisdnUseCase.kt
@@ -1,5 +1,6 @@
 package ventures.dvx.base.user.usecase
 
+import kotlinx.coroutines.reactor.awaitSingle
 import org.axonframework.eventhandling.EventHandler
 import org.axonframework.extensions.reactor.commandhandling.gateway.ReactorCommandGateway
 import org.springframework.stereotype.Component
@@ -8,11 +9,9 @@ import ventures.dvx.base.user.api.EndUserId
 import ventures.dvx.base.user.api.EndUserLoginStartedEvent
 import ventures.dvx.base.user.api.InvalidInput
 import ventures.dvx.base.user.api.LoginEndUserCommand
-import ventures.dvx.base.user.api.UserNotFoundError
-import ventures.dvx.base.user.command.EndUser
 import ventures.dvx.base.user.web.MsisdnLoginDto
-import ventures.dvx.common.axon.command.persistence.IndexJpaEntity
-import ventures.dvx.common.axon.command.persistence.IndexRepository
+import ventures.dvx.common.axon.security.runAsSuperUser
+import ventures.dvx.common.logging.LoggerDelegate
 import ventures.dvx.common.service.sms.SmsService
 import ventures.dvx.common.types.toErrStrings
 import ventures.dvx.common.workflow.UseCaseRunner
@@ -21,22 +20,21 @@ import ventures.dxv.base.user.error.UserException
 
 @Component
 class LoginByMsisdnUseCase(
-  private val indexRepository: IndexRepository,
-  val commandGateway: ReactorCommandGateway,
+  private val commandGateway: ReactorCommandGateway,
+  private val findUserByUsernameUseCase: FindUserByUsernameUseCase
 ) : UseCaseRunner<MsisdnLoginDto, Mono<EndUserId>> {
 
-  override suspend fun run(input: MsisdnLoginDto): Mono<EndUserId> = runAsyncMono {
-    val user: Mono<IndexJpaEntity> =
-      indexRepository.findEntityByAggregateNameAndKey(EndUser.aggregateName(), input.msisdn)
-        ?.let { Mono.just(it) }
-        ?: Mono.error(UserException(UserNotFoundError(input.msisdn)))
+  val log by LoggerDelegate()
 
-    user
-      .map { LoginEndUserCommand.of(EndUserId(it.id), it.key) }
-      .flatMap { it.fold(
+  override suspend fun run(input: MsisdnLoginDto): Mono<EndUserId> = runAsyncMono {
+    findUserByUsernameUseCase.run(input.msisdn)
+      .runAsSuperUser() // user is not logged in yet, so we have to run as superuser
+      .awaitSingle()
+      .let { LoginEndUserCommand.of(EndUserId(it.id), input.msisdn) }
+      .fold(
         { errors -> Mono.error(UserException(InvalidInput(errors.toErrStrings())))},
         { cmd -> Mono.just(cmd) }
-      )}
+      )
       .flatMap { commandGateway.send(it) }
   }
 
@@ -47,4 +45,5 @@ class LoginByMsisdnUseCase(
       event.token.msisdn.value,
       "Here is your DVX Application Token: ${event.token.token}"
     )
+
 }
