0c526a6526dab8e3ef5b0a1a83db5e76e7d6a332
Erik Dreyer <erik.dreyer@gmail.com>
2026-03-07 19:30:01 +0100

    Now users OSS version of ACL library

--- acl-spring-security/src/main/kotlin/io/liquidsoftware/common/security/spring/AuthenticationAccessSubjectResolver.kt
+++
@@ -1,8 +0,0 @@
-package io.liquidsoftware.common.security.spring
-
-import io.liquidsoftware.common.security.acl.AccessSubject
-import org.springframework.security.core.Authentication
-
-fun interface AuthenticationAccessSubjectResolver {
-  fun resolve(authentication: Authentication?): AccessSubject
-}
